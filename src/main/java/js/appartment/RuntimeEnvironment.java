48f159334deed02217e31121e948d38167c2e19e
NecOnIce <jonas.scherbaum@gmail.com>
2016-05-26 21:43:19 +0200

    added HtmlUnitDriver for Headless Browser support.

--- src/main/java/js/appartment/RuntimeEnvironment.java
+++ src/main/java/js/appartment/RuntimeEnvironment.java
@@ -1,9 +1,22 @@
 package js.appartment;
 
+import com.gargoylesoftware.htmlunit.IncorrectnessListener;
+import com.gargoylesoftware.htmlunit.InteractivePage;
+import com.gargoylesoftware.htmlunit.ScriptException;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HTMLParserListener;
+import com.gargoylesoftware.htmlunit.javascript.JavaScriptErrorListener;
 import js.appartment.pages.LoginPage;
 import js.appartment.pages.Page;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.w3c.css.sac.CSSException;
+import org.w3c.css.sac.CSSParseException;
+import org.w3c.css.sac.ErrorHandler;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * The RuntimeEnvironment encapsulates all Runtime specific Instances and variables as well as all State's necessary
@@ -21,7 +34,8 @@ public class RuntimeEnvironment {
      * Creates an instance of the RuntimeEnvironment
      */
     private RuntimeEnvironment() {
-        webDriver = new FirefoxDriver();
+        // webDriver = new FirefoxDriver();
+        webDriver = new HtmlUnitDriver();
 
         // ensure that the Browser is closed together with the Application
         Runtime.getRuntime().addShutdownHook(new Thread() {
